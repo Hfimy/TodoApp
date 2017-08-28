78f853348d807f6d66e14e73b11f49a5b6835a3c
Huang Tao(研发一部） <uid12403@sansi.com>
2017-08-28 08:50:20 +0800

    storeEnhancers

--- src/store.js
+++ src/store.js
@@ -1,11 +1,27 @@
-import {createStore,combineReducers} from 'redux'
+import {createStore,combineReducers,applyMiddleware,compose} from 'redux'
+
 
 import {reducer as todoReducer} from './todos'
 import {reducer as filterReducer} from './filter'
+
+import Perf from 'react-addons-perf'
+
+const win=window;
+win.Perf=Perf;
+
+
 const reducer =combineReducers({
     todos:todoReducer,
     filter:filterReducer
 })
 
+const middlewares=[];
+/*if(process.env.NODE_ENV!=='production'){
+    middlewares.push(require('redux-immutable-state-invariant')());
+}*/
+const storeEnhancers=compose(
+    applyMiddleware(...middlewares),
+    (win&&win.devToolsExtension)?win.devToolsExtension():(f)=>f
+)
 //1.createStore记得要设置初始值
-export default createStore(reducer,{})
+export default createStore(reducer,{},storeEnhancers);
